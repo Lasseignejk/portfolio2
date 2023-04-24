a1893ab9d7dbdd79534f500661466d1f040dcbb4
Lasseignejk <jaye914@hotmail.com>
2023-04-24 19:06:13 -0400

    added pictures of project homepages
    also wrote the information for avocado -- need to format

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,21 +1,20 @@
 import { ParallaxLayer } from "@react-spring/parallax";
-// import {
-// 	FaJsSquare,
-// 	FaHtml5,
-// 	FaCss3Alt,
-// 	FaGithub,
-// 	FaReact,
-// 	FaPython,
-// 	FaNode,
-// 	FaDocker,
-// 	FaFileDownload,
-// } from "react-icons/fa";
-// import {
-// 	SiTailwindcss,
-// 	SiTypescript,
-// 	SiPostgresql,
-// 	SiRedux,
-// } from "react-icons/si";
+import {
+	FaJsSquare,
+	FaHtml5,
+	FaCss3Alt,
+	FaGithub,
+	FaReact,
+	FaPython,
+	FaNode,
+	FaDocker,
+} from "react-icons/fa";
+import {
+	SiTailwindcss,
+	SiTypescript,
+	SiPostgresql,
+	SiRedux,
+} from "react-icons/si";
 
 const About = (): JSX.Element => {
 	return (
@@ -33,18 +32,38 @@ const About = (): JSX.Element => {
 					display: "flex",
 					padding: "2rem",
 					flexDirection: "column",
-					gap: "30px",
+					gap: "20px",
 				}}>
 				<header className="text-4xl font-bold text-themeYellow textShadow">
 					About Me
 				</header>
-				<div className="flex flex-col justify-evenly gap-10">
+				<div className="flex flex-col justify-evenly gap-[20px]">
 					<p className="text-themeLtText text-xl">
 						I am a confident communicator and motivated developer with an eye
 						for detail. I thrive in fast-paced environments and have the ability
 						to remain flexible and calm under pressure.
 					</p>
-					<div className="flex text-center gap-3 ">
+
+					<div className="mb-[20px]">
+						<header className="text-3xl text-themeYellow textShadow font-bold mb-[20px]">
+							Tech Stack
+						</header>
+						<div className="flex gap-3 text-5xl flex-wrap justify-center text-themeLtText">
+							<FaJsSquare />
+							<FaHtml5 />
+							<FaCss3Alt />
+							<FaGithub />
+							<FaReact />
+							<FaPython />
+							<FaNode />
+							<FaDocker />
+							<SiTailwindcss />
+							<SiTypescript />
+							<SiPostgresql />
+							<SiRedux />
+						</div>
+					</div>
+					<div className="flex text-center justify-center items-center gap-3">
 						<a
 							href="../../resume/JayelonLasseigneResume.pdf"
 							download
--- src/components/Projects/ProjectSlides.tsx
+++ src/components/Projects/ProjectSlides.tsx
@@ -5,25 +5,66 @@ import {
 	AnimatedProps,
 	useSpringRef,
 } from "@react-spring/web";
+import { FaGithub, FaYoutube } from "react-icons/fa";
 
 const pages: ((
 	props: AnimatedProps<{ style: CSSProperties }>
 ) => React.ReactElement)[] = [
 	({ style }) => (
-		<animated.div style={{ ...style, background: "#387f5f" }}>
-			<div>
+		<animated.div
+			style={{
+				...style,
+				background: "#387f5f",
+				padding: "2rem",
+				display: "flex",
+				flexDirection: "column",
+				gap: "20px",
+			}}>
+			<div className="">
 				<img src="/avocado_gray.png" />
+			</div>
+			<div>
+				<img
+					src="/AvocadoManageRestaurants.png"
+					className="border-2 border-themeDkText"
+				/>
+			</div>
+			<div>
+				<FaGithub />
+				<FaYoutube />
+			</div>
+			<div>
+				<h3>Description</h3>
+				<p>
+					Inspired by the POS software 'Toast', 'Avocado' is a site where
+					restaurant owners can view current orders and reports for their
+					restaurants. They can also edit their menus and upload photos. On the
+					opposite side, customers can view restaurants and place orders.{" "}
+				</p>
+			</div>
+			<div>
+				<h3>Role</h3>
+				<p>
+					As the project manager of a group of four, I participated in daily
+					standups with my instructors and led daily standups with my team. Not
+					only was I in charge of the styling of Avocado, based on mockups
+					created by my team, I also assisted and debugged code on both the
+					front and back ends and kept everyone organized using Figma and GitHub
+					Projects.
+				</p>
 			</div>
 		</animated.div>
 	),
 	({ style }) => (
-		<animated.div style={{ ...style, background: "#ff9d00" }}>
-			<h1 className="font-eggTitle text-eggText text-5xl">The Broken Egg</h1>
+		<animated.div style={{ ...style, background: "#ff9d00", padding: "2rem" }}>
+			<div>
+				<h1 className="font-eggTitle text-eggText text-3xl">The Broken Egg</h1>
+			</div>
 		</animated.div>
 	),
 	({ style }) => (
-		<animated.div style={{ ...style, background: "#f7dfff" }}>
-			<h1 className="font-readsTitle text-readsText text-5xl">BetterReads</h1>
+		<animated.div style={{ ...style, background: "#f7dfff", padding: "2rem" }}>
+			<h1 className="font-readsTitle text-readsText text-3xl">BetterReads</h1>
 		</animated.div>
 	),
 ];
